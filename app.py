3c3baae90d915013aaa267c9a7ef724ea5cd1233
CarrotCoding <84916659+CarrotCoding@users.noreply.github.com>
2021-07-05 17:17:38 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+# import models
+# main menu
+    # add, search, analysis, exit, view
+# functions
+    # add books to the database
+    # delete books
+    # search books
+    # data cleaning
+# loop runs program
+    # when user exits it stops
---
+++ models.py
@@ -0,0 +1,4 @@
+# create a database
+# give it a name books.db
+# create a model
+    # title, author, date published, price
